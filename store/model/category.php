56e879f944e46cb7394aab8a6ce76875c61eb590
Islam Magdy <DashaDeshozz@gmail.com>
2020-04-26 22:36:07 +0200

    model Changes

---
+++ store/model/category.php
@@ -0,0 +1,27 @@
+<?php
+  require_once(__ROOT__ . "model/Model.php");
+?>
+<?php 
+class category extends Model
+{
+    private $name;
+    private $subCategories;
+    function __construct()
+    {
+        
+    }
+    function setName($name)
+    {
+      $this->name = $name;
+    }
+    function getName($name)
+    {
+      $
+    }
+
+
+
+
+
+}
+?>
