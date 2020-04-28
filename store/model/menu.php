956acc06360bb5cb2cb54fcdfa96a9675fa9934a
Abdelazizmuhmd <41162973+Abdelazizmuhmd@users.noreply.github.com>
2020-04-28 20:34:41 +0200

    Update menu.php

--- store/model/menu.php
+++ store/model/menu.php
@@ -20,7 +20,8 @@ public function fillCategoriesArray(){
     if ($this->db->numRows()>0){
      categories = $this->db->getdata();
     foreach (categories as $value) {
-    array_push($this->categories, new category($value->id,$value->name));
+        $this->categories[]=new category($value->id,$value->name);
+  
     }
     }  
     
