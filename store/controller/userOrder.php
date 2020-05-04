be02215cb77b98db656ba4a35c8ebbfa3d859775
Islam Magdy <DashaDeshozz@gmail.com>
2020-05-05 01:12:16 +0200

    user order controller

---
+++ store/controller/userOrder.php
@@ -0,0 +1,18 @@
+<?php
+require_once("Controller.php");
+class userOrder extends Controller{
+ public function getOrders(){
+     $userid = $_REQUEST['userid'];
+     $this->model->getorders($userid);
+ }
+ public function getOrderDetails(){
+    $orderid = $_REQUEST['orderid'];
+    $this->model->getorderdetails($orderid);
+}
+public function updateOrder(){
+    $orderid = $_REQUEST['orderid'];
+    $status = $_REQUEST['status'];
+    $this->model->updateorder($orderid,$status);
+}
+}
+?>
