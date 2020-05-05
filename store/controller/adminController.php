7239ee46a3543fd7f03c08603dd38c1a42cca2e0
KhaledLoay99 <45739327+KhaledLoay99@users.noreply.github.com>
2020-05-05 21:47:51 +0200

    update AdminController 1

--- store/controller/adminController.php
+++ store/controller/adminController.php
@@ -46,11 +46,38 @@ function readProducts(){
     
 
 function  insertProduct(){
+
+    $name = $_REQUEST['name'];
+    $code = $_REQUEST['code'];
+    $cost = $_REQUEST['cost'];
+    $profit = $_REQUEST['profit'];
+    $description = $_REQUEST['description'];
+    $weight = $_REQUEST['weight'];
+    $productDetails = $_REQUEST['productDetails'];
+    $subcategoryid = $_REQUEST['subcategoryid'];
     $this->modal->getCategories()[0]->getSubcategories()[0]->getProducts()[0]->insertProduct();
     
 }
     
 function  updateProduct(){
+
+    $productid = $_REQUEST['productid'];
+
+    $productdetailid = $_REQUEST['productdetailid'];
+    $name = $_REQUEST['name'];
+    $code = $_REQUEST['code'];
+    $cost = $_REQUEST['cost'];
+    $profit = $_REQUEST['profit'];
+    $description= $_REQUEST['description'];
+    $weight= $_REQUEST['weight'];
+    $color= $_REQUEST['color'];
+    $s= $_REQUEST['s'];
+    $m= $_REQUEST['m'];
+    $l= $_REQUEST['l'];
+    $xl= $_REQUEST['xl'];
+    $xxl= $_REQUEST['xxl'];
+    $xxxl= $_REQUEST['xxxl'];
+    $imageurls= $_REQUEST['imageurls'];
     $this->modal->getCategories()[0]->getSubcategories()[0]->getProducts()[0]->updateProduct();
     
 }
