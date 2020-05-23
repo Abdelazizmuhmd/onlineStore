ac8585323ccc2ed0f66d7a2f1c6d4ce9d93f972c
KhaledLoay99 <45739327+KhaledLoay99@users.noreply.github.com>
2020-05-23 17:40:51 +0200

    responsive_header_done

--- store/public/header.php
+++ store/public/header.php
@@ -1,183 +1,68 @@
 <html>
 <head>
-    
-     <link href="../css/home.css" rel="stylesheet" type="text/css" media="all" /> 
-
-    
+   <link href="../css/home.css" rel="stylesheet" type="text/css" media="all" /> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  
     </head>
+
+ 
 <body>
-      <a href="" class="announcement-bar announcement-bar--link">
-    <p class="announcement-bar__message site-wrapper">WELCOME TO  PAT</p>
-    </a>
-    <div class="site-wrapper">
-        
+    
+<nav class="navbar navbar-expand-md navbar-light bg-success">
+    <a href="#" class="navbar-brand" style="color: white;">Bat Store</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="top-bar grid">
-
-<?php
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <?php
             session_start();
       
       if(!isset($_SESSION['usertype'])||$_SESSION['usertype']=="guest"){
-?>   
-      <div class="grid__item medium-up--two-fifths small--hide" style="margin-left: 140px;">
-
-        <span class="customer-links small--hide">
-
-            
-        <button onclick="movetologin()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_login_link">
-                Log in</a></button>
-            <span class="vertical-divider"></span>
-            <button onclick="movetosignup()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_register_link">Sign up</a>
-          </button>
-        </span>
-      </div>
-      <?php
+?>
+            <a href="#" onclick="movetologin()" class="nav-item nav-link active" style="color: white;">Login</a>
+            <a href="#" onclick="movetosignup()" class="nav-item nav-link" style="color: white;">Signup</a>
+            <?php
          }
-      else if($_SESSION['usertype']=="client"){
-      ?>
-      <div class="grid__item medium-up--two-fifths small--hide" style="margin-left: 140px;">
 
-        <span class="customer-links small--hide">
-            
-      <span style=" font-size:20px;color:#44BB09;font-weight:bold" >Welcome 
-        <?php   echo $_SESSION['name']; ?>
-            </span>
-            
-            <span class="vertical-divider"></span>
-            <button onclick="movetoHome()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_login_link">
-                Home</a></button>
-                        <span class="vertical-divider"></span>
+               else if($_SESSION['usertype']=="client"){
 
-            <button onclick="movetoorders()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_register_link">myOrders</a>
-          </button>
-
-            <button onclick="logout()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_login_link">
-                logout</a></button>
-         
-            
-            
-        </span>
-      </div>
-      
-      <?php }else if($_SESSION['usertype']=="admin"){ ?>
-      
-     <div class="grid__item medium-up--two-fifths small--hide" style="margin-left: 140px;">
-
-        <span class="customer-links small--hide">
-            
- <button onclick="movetoHome()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_login_link">
-                Home</a></button>
-
-        <button onclick="movetoorders()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_login_link">
-                Client Orders</a></button>
-            <button onclick="movetoadminproducts()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_register_link">AdminPanel</a>
-          </button>
-        </span>
-            <button onclick="movetoreport()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_register_link">Generate Report</a>
-          </button>  
-      
-
-         
-          <button onclick="logout()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-            <a href="" id="customer_login_link">
-                logout</a></button>
-         
-      </div>
-      
-      <?php }
-                       ?>
-      
-      
-      
-      
- <div class="grid__item  medium-up--two-fifths  small--one-half text-right">
-      <?php 
-            if(isset($_SESSION['usertype'])){
-            if($_SESSION['usertype']=="admin"){ 
-                ?>
-        <button onclick="system()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
-             <a href="" id="customer_login_link">
-                System Logs</a></button>
-            <?php 
-                
-                }}
          ?>
-           
-     <button onclick="movetocart()" style=" background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;">
+
+            <a class="nav-item nav-link active">Welcome 
+        <?php   echo $_SESSION['name']; ?></a>
+            <a href="#" onclick="movetoHome()" class="nav-item nav-link active" style="color: white;">Home</a>
+            <a href="#" onclick="movetoorders()" class="nav-item nav-link" style="color: white;">My Orders</a>
+            <a href="#" onclick="logout()" class="nav-item nav-link active" style="color: white;">Logout</a>
+<?php }
+
+
+
+
+
+
+
+                else if($_SESSION['usertype']=="admin"){ ?>
+            <a href="#" onclick="movetoHome()" class="nav-item nav-link active" style="color: white;">Home</a>
+            <a href="#" onclick="movetoorders()" class="nav-item nav-link" style="color: white;">Client Orders</a>
+            <a href="#" onclick="movetoadminproducts()" class="nav-item nav-link active" style="color: white;">Admin Panel</a>
+            <a href="#" onclick="movetoreport()" class="nav-item nav-link active" style="color: white;">Generate Report</a>
+            <a href="#" onclick="system()" class="nav-item nav-link" style="color: white;">System Logs</a>
+            <a href="#" onclick="logout()" class="nav-item nav-link active" style="color: white;">Logout</a>
+<?php } ?>
+
+        </div>
+        <div class="navbar-nav ml-auto">
          
+
+
          
-      <a onclick="" class="site-header__cart">
+      <a href="#" style="text-decoration: none;" onclick="movetocart()"class="nav-item nav-link" style="color: white;">
         <svg aria-hidden="true" focusable="false" role="presentation" class="icon icon-cart" viewBox="0 0 20 20"><path fill="#444" d="M18.936 5.564c-.144-.175-.35-.207-.55-.207h-.003L6.774 4.286c-.272 0-.417.089-.491.18-.079.096-.16.263-.094.585l2.016 5.705c.163.407.642.673 1.068.673h8.401c.433 0 .854-.285.941-.725l.484-4.571c.045-.221-.015-.388-.163-.567z"/><path fill="#444" d="M17.107 12.5H7.659L4.98 4.117l-.362-1.059c-.138-.401-.292-.559-.695-.559H.924c-.411 0-.748.303-.748.714s.337.714.748.714h2.413l3.002 9.48c.126.38.295.52.942.52h9.825c.411 0 .748-.303.748-.714s-.336-.714-.748-.714zM10.424 16.23a1.498 1.498 0 1 1-2.997 0 1.498 1.498 0 0 1 2.997 0zM16.853 16.23a1.498 1.498 0 1 1-2.997 0 1.498 1.498 0 0 1 2.997 0z"/></svg>
-        <span class="small--hide">
           Cart
     (<span id="CartCount"><?php if(isset($_COOKIE['cook'])){
     
@@ -188,14 +73,12 @@ $arr=json_decode($a, true);
 }else{
     echo 0;
 }  ?></span>)
-        </span>
       </a>
-         </button>
+        </div>
     </div>
-     </div>
-       
-
-        <script>
+</nav>
+</body>
+<script>
            
             function movetoreport(){
                 window.top.location.href = "http://localhost/onlineStore/store/public/report.php"; 
@@ -231,9 +114,5 @@ $arr=json_decode($a, true);
                  window.top.location.href = "http://localhost/onlineStore/store/other/errormasseges.php"; 
             }
         </script>
-        
-      <hr class="small--hide hr--border">
-    </div>
-</body>
 
 </html>
