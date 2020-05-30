a93c03a835daa7315617742118a239f3d0dde020
Ahmed Elbanna <59926177+elbanna962@users.noreply.github.com>
2020-05-30 18:16:29 +0200

    Most 3 products

--- store/public/report.php
+++ store/public/report.php
@@ -1,6 +1,11 @@
 <?php 
 require_once("../model/user.php");
 include_once("../other/session.php");
+require_once("../model/user.php");
+
+
+$model2 = new user();
+$model2->getMostSell();
 
 $model = new user();
 $model->generateReport();
@@ -11,9 +16,9 @@ $model->generateReport();
 <head>
 <style>
     h1{
-        color:#3DB8A4
+        color:#3DB8A4;
 
-;
+
         font-size: 50px;
           font-family: sans-serif;
 
@@ -34,6 +39,28 @@ text {
     .a{
         display: inline-block;
     }
+    .box{
+        position: relative;
+        display: inline-block; /* Make the width of box same as image */
+        margin-left:200px;
+        
+    }
+    .box .text{
+        position: absolute;
+        z-index: 999;
+        margin: 5 auto;
+        left: 0;
+        right: 0;        
+        text-align: center;
+        top: 40%; /* Adjust this value to move the positioned div up and down */
+        background: rgba(0, 0, 0, 0.8);
+        font-family: Arial,sans-serif;
+        color: #0000FF ;
+        width: 60%; /* Set the width of the positioned div */
+        
+        opacity: 0.8;
+        /* #fff; */
+    }
 </style>    
 </head>
 <body>
@@ -51,9 +78,11 @@ text {
           
           
           echo $model->getreport()->getcost();  ?> L.E</text>
+         
     </g>
   </svg>
-</div>     
+</div>   
+  
 &nbsp;
 &nbsp;
 &nbsp;
@@ -78,10 +107,67 @@ text {
           
           
           echo $model->getreport()->getprofit();  ?> L.E</text>
+          
     </g>
   </svg>
+ 
             </div>
+            
     </center>
+    <br>;
+            <br>;
+            <br>;
+            <br>;
+            <br>;
+            <br>;
+            <br>;
+            
+    <?php
+           $a=$model2->getSell()->getSelled();
+          $b=$model2->getSell()->getImage();
+         
+
+
+         
+          for($j=0;$j<3;$j++)
+          {
+            $imgs_arr = [];
+            $imgs_arr=unserialize($b[$j]);
+            
+            $count2 = count($imgs_arr);
+            for ($i=0;$i<$count2;$i++){
+      
+  
+
+
+
+  $bana="$imgs_arr[$i]";
+
+  echo"
+  <div class='box'>
+  <img src='{$imgs_arr[$i]}grande.jpeg' >
+  ";
+ 
+  
+
+            
+
+                       }
+                       echo"<div class='text'>
+<h1 style='color:white;'>$a[$j]</h1>
+</div>
+</div>
+";
+         
+           
+          }
+
+            
+        
+          
+
+
+          ?>
 </body>
         
 
