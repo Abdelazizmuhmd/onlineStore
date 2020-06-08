5bf909e2d02266450dc7d7232c22966d06edc853
Islam Magdy <DashaDeshozz@gmail.com>
2020-06-08 23:38:33 +0200

    isset validation

--- store/controller/userOrder.php
+++ store/controller/userOrder.php
@@ -3,43 +3,49 @@ require_once("Controller.php");
 class userOrderController extends Controller{
  public function getOrders(){
 
- 	 //$userid = 1;
-     $userid = $_REQUEST['userid'];
+      //$userid = 1;
+     if(isset($_REQUEST['userid']))
+      $userid = $_REQUEST['userid'];
      $this->model->getorders($userid);
  }
  public function getOrderDetails(){
-    $orderid = $_REQUEST['orderid'];
+    if(isset($_REQUEST['orderid']))
+     $orderid = $_REQUEST['orderid'];
     $this->model->getorderdetails($orderid);
 }
 public function updateOrder(){
-    $orderid = $_REQUEST['orderid'];
-    $status = $_REQUEST['status'];
+    if(isset($_REQUEST['orderid']))
+     $orderid = $_REQUEST['orderid'];
+    if(isset($_REQUEST['status']))
+     $status = $_REQUEST['status'];
     $this->model->updateorder($orderid,$status);
 }
 public function getOrder(){
-    $orderid=$_REQUEST['orderid'];
+    if(isset($_REQUEST['orderid']))
+     $orderid=$_REQUEST['orderid'];
     $this->model->getordersArray()[0]->readOrder($orderid);
 
 }
     
 public function getuser(){
-
-    $userid=$_REQUEST['userid'];
+    if(isset($_REQUEST['userid']))
+     $userid=$_REQUEST['userid'];
     $this->model->getuser($userid);
     
 }
 public function delete(){
-    
-    $orderid=$_REQUEST['orderid'];
-
+    if(isset($_REQUEST['orderid']))
+     $orderid=$_REQUEST['orderid'];
     $this->model->getordersArray()[0]->delete($orderid);
     header("location:../public/orders.php");
     
     
 }
 public function updatestat(){
-    $orderid=$_REQUEST['orderid'];
-    $status=$_REQUEST['stat'];
+    if(isset($_REQUEST['orderid']))
+     $orderid=$_REQUEST['orderid'];
+    if(isset($_REQUEST['stat']))
+     $status=$_REQUEST['stat'];
     $this->model->getordersArray()[0]->updateStat($orderid,$status);
     header("location:../public/orders.php");
 
